05a0deabe8cdb28c73cec845c1a6896a8ec57210
oscarach30 <oscarach30@gmail.com>
2022-06-01 01:57:18 -0700

    jk

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './brand.css'
+import React from 'react';
+import './brand.css';
 
 
 const brand = () => {
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+
 import './navbar.css';
-import {RiMenu3Line, RiCloseLin} from 'react-icons/ri';
+
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
+
 import logo from '../../assets/logo.svg';
 
 
