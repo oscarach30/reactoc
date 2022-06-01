b73558aec259e172f109fb4a78a14b585e256fc7
oscarach30 <oscarach30@gmail.com>
2022-06-01 01:52:23 -0700

    commit

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './article.css'
+
+
+const Article = () => {
+  return (
+    <div>
+      <h1>Article</h1>
+        </div>
+  )
+}
+
+export default Article
---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './brand.css'
+
+
+const brand = () => {
+  return (
+    <div>
+      <h1>Brand</h1>
+    </div>
+  )
+}
+
+export default brand
